6baf63a9dec24452f8ed5c1097bbe749c355d904
ike3232 <iketonijon@gmail.com>
2024-05-09 14:34:04 +0100

    fixed code

---
+++ provider.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "5.28.0"
+    }
+  }
+}
+
+provider "google" {
+  # Configuration options
+  provider "google" 
+  project     = "test-422618"
+  region      = "us-central1"
+  zone        = "us-central1-a"
+  credentials = file("/tmp/credentials.json")
+}
